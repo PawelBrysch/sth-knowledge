42d3aa6a1541d6684fddcffe5d256a699584a9db
brycho94 <pawel.brysch@gmail.com>
2019-12-17 19:14:32 +0100

    Before investigation.

--- top_django/less_scripts.cmd
+++ top_django/less_scripts.cmd
@@ -4,5 +4,15 @@ django-admin startproject mysite
 REM run server (dev)
 python mysite\manage.py runserver
 
-REM create app
+REM create app (boilerplate)
+python manage.py startapp my_first_app
+
+REM ####################################
+REM Moving model to DB.
+REM ####################################
+
+REM migrate data do DB
+python manage.py makemigrations my_first_app
+
+REM migrate data do DB
 python manage.py startapp my_first_app
--- top_django/mysite/mysite/settings.py
+++ top_django/mysite/mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'my_first_app.apps.MyFirstAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -73,10 +74,21 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'for_django',
+        'USER': 'devoted',
+        'PASSWORD': 'Janina123',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
@@ -105,7 +117,8 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+# TIME_ZONE = 'Europe/Berlin'
+TIME_ZONE = 'Europe/Berlin'
 
 USE_I18N = True
 
