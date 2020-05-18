e55f3a1877557aa0f5de328e8338cc7792c7f988
brycho94 <pawel.brysch@gmail.com>
2020-05-18 08:52:37 +0200

    Linear regression stopped. Returned to drf.

--- source/math_linreg2/main.py
+++ source/math_linreg2/main.py
@@ -1,1 +1,17 @@
 from sklearn.linear_model import LinearRegression
+import pandas as pd
+import os
+
+df = pd.read_excel(
+    io=os.path.abspath("data_preparing.xlsx"),
+    header=0,
+    usecols="C:F",
+    nrows=20
+)
+
+X = df.iloc[:, :-1]
+y = df["calories"]
+
+reg = LinearRegression().fit(X, y)
+print(reg.coef_)
+
