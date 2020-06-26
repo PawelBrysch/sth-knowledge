1331df98fc23184b9cce39303d3d884654126a26
unknown <you@example.com>
2020-06-26 12:02:38 +0200

    bobas

--- mini_projects/proj_latex/main.py
+++ mini_projects/proj_latex/main.py
@@ -1,10 +1,13 @@
 import os
 import os.path as op
-from shutil import copyfile
 import shutil
 from pathlib import Path
 from pdf2image import convert_from_path
 import PIL
+import subprocess
+import sys
+import argparse
+
 
 def convert_from_pdf_to_png(path_to_pdf, path_to_png):
     pages = convert_from_path(path_to_pdf, 500)
@@ -20,17 +23,38 @@ def trim_png(path_to_input, path_to_output):
     if bbox:
         im.crop(bbox).save(path_to_output)
 
-path_to_input = rf"C:\Users\devoted_w\Documents\snippets_tops\sth-knowledge\mini_projects\proj_latex\input\test2.tex"
-input_filename = op.basename(path_to_input)
-cwd = op.abspath(op.join(path_to_input, op.pardir, op.pardir))
-path_to_tempdir = op.join(cwd, "temp")
+def convert_tex_to_trimmedpng(path_to_input):
+    # paths refs
+    # path_to_input = rf"C:\Users\devoted_w\Documents\snippets_tops\sth-knowledge\mini_projects\proj_latex\input\test2.tex"
+    input_filename = op.basename(path_to_input)
+    cwd = op.abspath(op.join(path_to_input, op.pardir, op.pardir))
+    path_to_tempdir = op.join(cwd, "temp")
+    path_to_input_copy = op.join(path_to_tempdir, input_filename)
+    path_to_intermediate = Path(path_to_input_copy).with_name("intermediate").with_suffix(".png")
+    path_to_output = Path(cwd).joinpath("output").joinpath(input_filename).with_suffix(".png")
 
-os.mkdir(path_to_tempdir)
-path_to_input_copy = op.join(path_to_tempdir, input_filename)
-copyfile(path_to_input, path_to_input_copy)
-os.system(f'cd temp && pdflatex -shell-escape {input_filename}')
+    # proper work
+    os.mkdir(path_to_tempdir)
+    shutil.copyfile(path_to_input, path_to_input_copy)
+    os.system(f'cd temp && pdflatex -shell-escape {input_filename}')
+    convert_from_pdf_to_png(Path(path_to_input_copy).with_suffix(".pdf"), path_to_intermediate)
+    trim_png(path_to_intermediate, path_to_output)
+    shutil.rmtree(path_to_tempdir)
+    subprocess.Popen(rf'explorer /select,"{path_to_output}"')
 
-path_to_intermediate = Path(path_to_input_copy).with_name("intermediate").with_suffix(".png")
-convert_from_pdf_to_png(Path(path_to_input_copy).with_suffix(".pdf"), path_to_intermediate)
-trim_png(path_to_intermediate, Path(cwd).joinpath("output").joinpath(input_filename).with_suffix(".png"))
-shutil.rmtree(path_to_tempdir)
+
+def parse_arguments(input_):
+    input_ = input_[1:]
+    parser = argparse.ArgumentParser()
+    parser.add_argument('path_to_input', type=str)
+    return parser.parse_args(input_)
+
+
+def main(input):
+    parsed_args = parse_arguments(input)
+    convert_tex_to_trimmedpng(**vars(parsed_args))
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
