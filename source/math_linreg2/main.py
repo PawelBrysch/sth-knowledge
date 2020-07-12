fc8a2c395c3d4f2fa7a3ea34497e56f37279981c
Pawel Brysch <pawel.brysch@gmail.com>
2020-07-12 10:21:42 +0200

    testy poprawione

--- source/math_linreg2/main.py
+++ source/math_linreg2/main.py
@@ -19,8 +19,6 @@ class ModelWrapper:
         self._output_column = output_column
 
     def __repr__(self):
-        # ", ".join(list(self._df)[:-1])
-        # return f"{self.value_sklearn.coef_} {self.value_sklearn.intercept_}"
         return "{:<45} {}".format(
             ", ".join(list(self._df)[:-1]),
             str(self.value_sklearn.coef_) + " " + str(self.value_sklearn.intercept_)
@@ -47,47 +45,18 @@ class ModelWrapper:
     def create_similar(self, column_to_exclude: str):
         return ModelWrapper.ModelWrapperFactory.create(self, column_to_exclude)
 
-        # def build(self, column_to_exclude: str):
-        #     new_df = self._df.drop([column_to_exclude], axis="columns")
-        #     new_model = ModelWrapper(new_df, self._output_column)
-        #     return new_model
 
+if __name__ == '__main__':
+    df_calories = pd.read_excel(io=os.path.abspath("data/calories.xlsx"), header=0, usecols="C:F", nrows=80)
+    df_density_big = pd.read_excel(io=os.path.abspath("data/confoundOmitted_bigIntercept.xlsx"), header=0,
+                                   usecols="C:E", nrows=80)
+    df_density_small = pd.read_excel(
+        io=os.path.abspath("data/nearlyPerfectlyCorrelatedConfoundOmitted_smallIntercept.xlsx"), header=0,
+        usecols="C:F", nrows=80)
 
+    model_calories = ModelWrapper(df_calories, "calories")
+    model_density_big = ModelWrapper(df_density_big, "bone density")
+    model_density_small = ModelWrapper(df_density_small, "bone density")
 
-
-# TODO z tego zrob funkcji, ktora generuje następny model
-class ExperimentIncludedExcluded:
-
-    def __init__(self, df, output_column, column_to_exclude=None):
-        self.df_included = df
-        self.model_included = ModelWrapper(self.df_included, output_column)
-        self.model_excluded = None
-        if column_to_exclude is not None:
-            self.df_excluded = df.drop([column_to_exclude], axis="columns")
-            self.model_excluded = ModelWrapper(self.df_excluded, output_column)
-
-    # TODO przenies to do wrappera jako compare with
-    def __repr__(self):
-        regs = [model.value_sklearn for model in [self.model_included, self.model_excluded] if model is not None]
-        reprs = [f"{reg.coef_} {reg.intercept_}" for reg in regs]
-        return "\n".join(reprs)
-
-
-df_calories = pd.read_excel(io=os.path.abspath("data/calories.xlsx"), header=0, usecols="C:F", nrows=80)
-df_density_big = pd.read_excel(io=os.path.abspath("data/confoundOmitted_bigIntercept.xlsx"), header=0, usecols="C:E", nrows=80)
-df_density_small = pd.read_excel(io=os.path.abspath("data/nearlyPerfectlyCorrelatedConfoundOmitted_smallIntercept.xlsx"), header=0, usecols="C:F", nrows=80)
-
-
-model_calories = ModelWrapper(df_calories, "calories")
-model_density_big = ModelWrapper(df_density_big, "bone density")
-model_density_small = ModelWrapper(df_density_small, "bone density")
-
-model_density_big_wo_weight = model_density_big.create_similar("weight")
-model_density_small_wo_years = model_density_small.create_similar("years")
-
-
-exp_calories = ExperimentIncludedExcluded(df_calories, "calories")
-exp_density_big = ExperimentIncludedExcluded(df_density_big, "bone density", "weight")
-exp_density_small = ExperimentIncludedExcluded(df_density_small, "bone density", "years")
-
-exp_density_big.model_excluded.value_statsmodels.summary()
+    model_density_big_wo_weight = model_density_big.create_similar("weight")
+    model_density_small_wo_years = model_density_small.create_similar("years")
--- source/math_linreg2/tests/test_sth.py
+++ source/math_linreg2/tests/test_sth.py
@@ -1,4 +1,4 @@
-from math_linreg2.main import ExperimentIncludedExcluded, ModelWrapper
+from math_linreg2.main import ModelWrapper
 from unittest.mock import patch
 import pandas as pd
 import filecmp
@@ -14,23 +14,25 @@ class TestName:
             io=os.path.abspath("data/nearlyPerfectlyCorrelatedConfoundOmitted_smallIntercept.xlsx"), header=0,
             usecols="C:F", nrows=80)
 
-        exp_calories = ExperimentIncludedExcluded(df_calories, "calories")
-        exp_density_big = ExperimentIncludedExcluded(df_density_big, "bone density", "weight")
-        exp_density_small = ExperimentIncludedExcluded(df_density_small, "bone density", "years")
+        model_calories = ModelWrapper(df_calories, "calories")
+        model_density_big = ModelWrapper(df_density_big, "bone density")
+        model_density_big_wo_weight = model_density_big.create_similar("weight")
+        model_density_small = ModelWrapper(df_density_small, "bone density")
+        model_density_small_wo_years = model_density_small.create_similar("years")
 
-        res2 = repr(exp_density_big)
-        res3 = repr(exp_density_small)
+        assert repr(model_calories) == "height, weight, iq                            [3.84990315 8.92499051 2.19968337] 250.94950350079193"
+        assert repr(model_density_big) == "weight, activity                              [0.58707936 0.83484872] 2.8340167340040523"
+        assert repr(model_density_big_wo_weight) == "activity                                      [0.18930451] 90.69592806454129"
+        assert repr(model_density_small) == "activity, weigth, years                       [-0.54460774  0.7615358   0.93237663] 4.0797312153622585"
+        assert repr(model_density_small_wo_years) == "activity, weigth                              [0.5649755  0.76466156] 7.673955643254075"
 
-        assert repr(exp_calories) == "[3.84990315 8.92499051 2.19968337] 250.94950350079193"
-        assert repr(exp_density_big) == "[0.58707936 0.83484872] 2.8340167340040523\n[0.18930451] 90.69592806454129"
-        assert repr(exp_density_small) == "[-0.54460774  0.7615358   0.93237663] 4.0797312153622585\n[0.5649755  0.76466156] 7.673955643254075"
 
    # TODO wpisac gdzies zmagania z tad
 
     @patch("yellowbrick.base.plt.show")
     def test_visualise(self, mock_show):
         df_calories = pd.read_excel(io=os.path.abspath("data/calories.xlsx"), header=0, usecols="C:F", nrows=80)
-        exp_calories = ExperimentIncludedExcluded(df_calories, "calories")
+        exp_calories = ModelWrapper(df_calories, "calories")
 
         output_path = os.path.abspath("temp/received.png")
         expected_path = os.path.abspath("data/expected.png")
@@ -39,7 +41,7 @@ class TestName:
         except:
             pass
 
-        retval_out = exp_calories.model_included.residuals_against_fitted()
+        retval_out = exp_calories.residuals_against_fitted()
         retval_out.fig.savefig(output_path)
 
         assert filecmp.cmp(expected_path, output_path)
