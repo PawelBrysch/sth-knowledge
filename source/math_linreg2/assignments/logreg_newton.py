bdf591ba33116e8f25726b86cf4f9a55f1bfc124
Pawel Brysch <pawel.brysch@gmail.com>
2020-08-16 16:18:16 +0200

    Before step refactoring

--- source/math_linreg2/assignments/logreg_newton.py
+++ source/math_linreg2/assignments/logreg_newton.py
@@ -1,60 +1,24 @@
-print("elo")
 import pathlib as pl
 import pandas as pd
 import numpy as np
 from typing import List
 
-x = pl.Path(__file__).with_name('x.dat')
-y = pl.Path(__file__).with_name('y.dat')
-pt1 = pd.read_csv(x, sep=r'\s{2,}', engine='python')
-pt2 = pd.read_csv(y, sep=r'\s{2,}', engine='python')
-data = pt2.assign(X1=pt1['X1'], X2=pt1['X2'])
-
-
-# X_avg = [-0.065, 0.015]
-X_avg = [5, 5]
-TAU = 0.5
-
-# def weights(X1: pd.Series, X2: pd.Series, X: List[int]):
-#     global TAU
-#     return np.exp(-np.sqrt((X1 - X[0]) ** 2 + (X2 - X[1]) ** 2) / (2 * TAU ** 2))
-
-def weights(X: pd.DataFrame, arg_x: List[int]):
-    global TAU
-    return np.exp(-np.sqrt((X['X1'] - arg_x[0]) ** 2 + (X['X2'] - arg_x[1]) ** 2) / (2 * TAU ** 2))
-
-small_testdata = pd.DataFrame.from_dict({
-    'Y': [0, 0],
-    'X1': [2, 1],
-    'X2': [3, 4],
-})
-big_testdata = pd.DataFrame.from_dict({
-    'Y': np.linspace(0, 0, 10),
-    'X1': np.linspace(1, 10, 10),
-    'X2': np.linspace(11, 20, 10),
-})
-"""TEST: weights"""
-# w = weights(small_testdata['X1'], small_testdata['X2'], X_avg)
-# small_testdata = small_testdata.assign(W=w)
-# print(small_testdata)
-"""TEST: weights"""
-# w = weights(small_testdata, X_avg)
-# small_testdata = small_testdata.assign(W=w)
-# print(small_testdata)
-# pd.testing.assert_series_equal(w, pd.Series([0.00073834, 0.00026225]))
-# pd.testing.assert_series_equal(pd.Series([0.00073834, 0.00026225]), pd.Series([0.00073834, 0.00026225]))
-
-
-# X = big_testdata.loc[:, ['X1', 'X2']]
-# TODO
 """notatka"""
 # X.__matmul__(X.T)
 # np.identity(X.__len__())
 
+x = pl.Path(__file__).with_name('x.dat')
+y = pl.Path(__file__).with_name('y.dat')
+pt1 = pd.read_csv(x, sep=r'\s{2,}', engine='python')
+pt2 = pd.read_csv(y, sep=r'\s{2,}', engine='python')
+
+
+
 
 class Step:
-    def __init__(self):
-        pass
+    @staticmethod
+    def _w(X1: pd.Series, X2: pd.Series, arg_x: List[int], tau: float):
+        return np.exp(-np.sqrt((X1 - arg_x[0]) ** 2 + (X2 - arg_x[1]) ** 2) / (2 * tau ** 2))
 
     @staticmethod
     def _h(X1: pd.Series, X2: pd.Series, theta: np.array):
@@ -86,5 +50,53 @@ class Step:
     def _theta_1(theta_0: np.array, H: np.array, gradient: np.array):
         return theta_0 - np.linalg.inv(H).__matmul__(gradient)
 
+    def __init__(self, data, theta0, lambda_, tau, x):
+        self.X = data.loc[:, ['X0', 'X1', 'X2']]
+        self.X1 = data['X1']
+        self.X2 = data['X2']
+        self.Y = data['Y']
+        self.lambda_ = lambda_
+        self.tau = tau
+        self.x = x
+
+        self.theta0 = theta0
 
 
+    def next_theta(self):
+        w = Step._w(self.X1, self.X2, self.x, self.tau)
+        h = Step._h(self.X1, self.X2, self.theta0)
+        z = Step._z(h, self.Y, w)
+        gradient = Step._gradient(self.X, z, self.theta0, self.lambda_)
+        D = Step._D(h, w)
+        H = Step._H(self.X, D, self.lambda_)
+        next_theta = Step._theta_1(self.theta0, H, gradient)
+
+        return next_theta
+
+class Algorithm:
+    def __init__(self, data, theta0, lambda_, tau, x):
+        self.X = data.loc[:, ['X0', 'X1', 'X2']]
+        self.X1 = data['X1']
+        self.X2 = data['X2']
+        self.Y = data['Y']
+        self.lambda_ = lambda_
+        self.tau = tau
+        self.x = x
+
+        self.steps = []
+        self.step1 = Step(data=data_, theta0=np.array([1, 1, 1]), lambda_=0.0001, tau=0.5, x=x_avg)
+        self.theta0 = theta0
+
+
+data_ = pt2.assign(X1=pt1['X1'], X2=pt1['X2'])
+data_ = data_.assign(X0=pd.Series(np.ones(data_.shape[0])))
+x_avg = [-0.065, 0.015]
+
+
+algo = Algorithm(data=data_, theta0=np.array([1, 1, 1]), lambda_=0.0001, tau=0.5, x=x_avg)
+res = algo.step1.next_theta()
+print(res)
+
+# step1 = Step(data=data_, theta0=np.array([1, 1, 1]), lambda_=0.0001, tau=0.5, x=x_avg)
+# res = step1.next_theta()
+# print(res)
--- source/math_linreg2/assignments/test_logger_newton.py
+++ source/math_linreg2/assignments/test_logger_newton.py
@@ -1,16 +1,18 @@
 import pytest
-from .logreg_newton import Step, weights
-# from .logreg_newton import Step, weights, func_H
+from .logreg_newton import Step
 import pandas as pd
 import numpy as np
 
 
 class TestName:
-    # def test_func_H(self):
-    #     THETA_0 = np.array([-1, 2, 1])
-    #
-    #     assert func_H(2, 3, THETA_0) == 0.9975273768433653
-    #     assert func_H(1, 4, THETA_0) == 0.9933071490757153
+    def test_w(self):
+        X1_ = pd.Series([2, 1])
+        X2_ = pd.Series([3, 4])
+        x_ = [5, 5]
+        tau_ = 0.5
+
+        res = Step._w(X1_, X2_, x_, tau_)
+        pd.testing.assert_series_equal(res, pd.Series([0.00073834, 0.00026225]))
 
     def test_h(self):
         THETA_0 = np.array([-1, 2, 1])
@@ -22,50 +24,50 @@ class TestName:
 
 
     def test_D(self):
-        h1 = pd.Series([4, 5, 6])
-        w1 = pd.Series([3, 2, 1])
+        h = pd.Series([4, 5, 6])
+        w = pd.Series([3, 2, 1])
 
-        res = Step._D(h1, w1)
+        res = Step._D(h, w)
         np.testing.assert_array_equal(res, np.array([[36,  0,  0], [0, 40,  0], [0,  0, 30]]))
 
 
     def test_H(self):
-        X1 = pd.DataFrame.from_dict({
+        X = pd.DataFrame.from_dict({
             'X1': [1, 3, 5],
             'X2': [2, 4, 6],
         })
-        D1 = np.diag([1, 2, -1])
+        D = np.diag([1, 2, -1])
         lambda1 = 0.5
 
-        res = Step._H(X1, D1, lambda1)
+        res = Step._H(X, D, lambda1)
         np.testing.assert_array_equal(res, np.array([[-6.5, -4.], [-4., -0.50]]))
 
     def test_z(self):
-        h1 = pd.Series([1, 2, 3])
-        y1 = pd.Series([6, 5, 4])
-        w1 = pd.Series([2, 4, 6])
+        h = pd.Series([1, 2, 3])
+        y = pd.Series([6, 5, 4])
+        w = pd.Series([2, 4, 6])
 
-        res = Step._z(h1, y1, w1)
+        res = Step._z(h, y, w)
         np.testing.assert_array_equal(res, np.array([10, 12, 6]))
 
     def test_gradient(self):
-        X1 = pd.DataFrame.from_dict({
+        X = pd.DataFrame.from_dict({
             'X1': [1, 3, 5],
             'X2': [2, 4, 6],
         })
-        z1 = pd.Series([1, 0, -1])
-        theta1 = np.array([-2, 2])
-        lambda1 = 0.5
+        z = pd.Series([1, 0, -1])
+        theta = np.array([-2, 2])
+        lambda_ = 0.5
 
-        res = Step._gradient(X1, z1, theta1, lambda1)
+        res = Step._gradient(X, z, theta, lambda_)
         np.testing.assert_array_equal(res, np.array([-3., -5.]))
 
     def test_theta_1(self):
-        H1 = np.array([[1, 2], [3, 4]])
-        gradient1 = np.array([1, -1])
-        theta_00 = np.array([1, 1])
+        H = np.array([[1, 2], [3, 4]])
+        gradient = np.array([1, -1])
+        theta_0 = np.array([1, 1])
 
-        res = Step._theta_1(theta_00, H1, gradient1)
+        res = Step._theta_1(theta_0, H, gradient)
         np.testing.assert_array_almost_equal(res, np.array([4., -1.]))
 
 
